a5afde7ee502b49ddc9325cea2943e18f0281f18
marcelopl82 <marceo.8468@gmail.com>
2024-06-25 20:23:15 -0300

    1 aula math

---
+++ java scripts. pt 3/mach.js/1.mach.js
@@ -0,0 +1,18 @@
+/*math== matemática
+pow= (2,2)poténcia / 2²= 4
+sqrt(25,2)/raiz/ 25
+pi
+ceil/teto= arrendodar para cima
+floor/ chão = arrendodar para baixo
+ 
+
+random()/ números aliatórios entre 0 , 1*/
+
+const result = Math.pow(2,3)
+
+
+
+console.log(result)
+
+
+
