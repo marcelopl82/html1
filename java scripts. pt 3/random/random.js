7198418ffc285caf412525aafb71dce906735c82
marcelopl82 <marceo.8468@gmail.com>
2024-06-28 21:19:32 -0300

    random aula 3 28/06/24

--- java scripts. pt 3/random/random.js
+++ java scripts. pt 3/random/random.js
@@ -1,6 +1,8 @@
 function generatenumber(){
-    const Men=document.querySelector(".input-min")
+    const Min=  Math.ceil(document.querySelector(".input-min").value )
     
-    const Max=document.querySelector(".input-max")
+    const Max=Math.floor(document.querySelector(".input-max").value);
+    const result= Math.floor(Math.random() * (Max - Min + 1)) + Min;
 
+    alert(result)
 }
