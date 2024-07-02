3e2f63a8b1c7c5eb7a8c0685eaca796ac8587cac
marcelopl82 <marceo.8468@gmail.com>
2024-07-02 19:44:14 -0300

    atribuição 02/07/24

--- java scripts. pt 3/math.js/1.mach.js
+++ java scripts. pt 3/math.js/1.mach.js
@@ -8,17 +8,40 @@ floor/ chão = arrendodar para baixo
 
 random()/ números aliatórios entre 0 , 1
 
-const result = Math.pow(2,3)*/
+const result = Math.pow(2,3)
 
 
 
 let result= 20
 result=result+90
 console.log(result)
+ 
 
+operadores de atribuição
+==> compara o valor// mas nao o tipo
 
+=== -> totalmete igual//compara o tipo eo valor
 
+!= -> diferente //ele compara o valor mas nao o tipo
+!== -> totalmente diferente//compara o valor eo tipo
+ const firstnumber= 30
+ const seccontnumber=30
 
+ if(firstnumber==seccontnumber){
+ console.log("sao iguais")}else{console.log("não sao iguais")}
+ 
+ operadores de comparação
+
+ > maior
+ < menor
+ >= maior ou igual
+ <=menor ou igual
+
+ */
+
+const firstnumber=30
+const seccontnumber=30
+console.log(seccontnumber>=firstnumber)
 
 
 
