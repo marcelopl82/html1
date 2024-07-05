42a2ad9260c5c23469a82ace93c85324b4f0ed31
marcelopl82 <marceo.8468@gmail.com>
2024-07-05 19:50:09 -0300

    m

--- java scripts. pt 3/random/random.js
+++ java scripts. pt 3/random/random.js
@@ -1,10 +1,20 @@
-function generatenumber(){
-    const Min=  Math.ceil(document.querySelector(".input-min").value )
-    
-    const Max=Math.floor(document.querySelector(".input-max").value);
-    const result= Math.floor(Math.random() * (Max - Min + 1)) + Min;
+function generatenumber() {
+    const Min = Math.ceil(document.querySelector(".input-min").value)
 
-    
+    const Max = Math.floor(document.querySelector(".input-max").value);
+
+    if (Min >= Max) {
+        alert("o valor MINIMO tem que ser MENOR que o MÁXIMO")
+
+    } else {
+
+
+
+        const result = Math.floor(Math.random() * (Max - Min + 1)) + Min;
     
 
-    alert(result  )}
+
+
+
+    alert(result)}
+}
