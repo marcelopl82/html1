ac3a69c6d5c67581a9089ef9c46c2ffca70cea63
marcelopl82 <marceo.8468@gmail.com>
2024-07-07 18:51:56 -0300

    s

---
+++ java scripts. pt 3/random/operadores lógicos.js
@@ -0,0 +1,19 @@
+//console.log(!true)
+
+const digiteasenha = false
+const digitetoken = false
+const digetenúmerodaconta = true
+
+
+/* if   (digiteasenha || digitetoken || digetenúmerodaconta){ console.log("Logado com sucesso")
+
+ }
+
+  else     { console.log("Não logado informação errada") }*/
+
+  if   (digiteasenha && digitetoken && digetenúmerodaconta){ console.log("Logado com sucesso")
+
+  }
+ 
+   else     { console.log("Não logado informação errada") }
+
