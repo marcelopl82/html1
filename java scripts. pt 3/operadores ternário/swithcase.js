7a7384245ecdff324b8c28ab44121eb437f9ed87
marcelopl82 <marceo.8468@gmail.com>
2024-07-12 20:21:04 -0300

    javascript 12/07/24 fim

---
+++ java scripts. pt 3/operadores ternário/swithcase.js
@@ -0,0 +1,21 @@
+/*switch case- controlador de fluxo
+
+const tempeteture= 30
+
+
+switch(tempeteture){
+case  10: 
+console.log("esta frio")
+break ;
+case  20: 
+console.log("está meio quente")
+break ;
+case  30: 
+console.log("esta quente")
+break ;
+
+
+default:
+    console.log("nao sei te diser")
+
+break}*/
