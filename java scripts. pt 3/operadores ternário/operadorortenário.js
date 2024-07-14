7c4b9cea5a2c71c791b1af6d94835765cdf58ea2
marcelopl82 <marceo.8468@gmail.com>
2024-07-14 20:27:23 -0300

    PARTE 4JS

--- java scripts. pt 3/operadores ternário/operadorortenário.js
+++ java scripts. pt 3/operadores ternário/operadorortenário.js
@@ -2,10 +2,11 @@ const salary = 10000
 //if (salary < 3000) { console.log("colaborador junior") }
 // else{ console.log("colaborador senior") }
 
-//salary > 3000 ? console.log("colaborador junior") : console.log("colaborador senior")/*só usa em uma linha de código*/
+//salary > 3000 ? console.log("colaborador junior") : console.log("colaborador senior")/*só usa em uma linha de código
 
 //if salary < 3000 && console.log("colaborador junior")
 
 //if else ->salary > 3000 ? console.log ("colaborador junior") : console.log ("ele é senior")
 // else if 
-salary > 3000 ? console.log("colaborador junior") : salary >= 3000 && salary < 10000 ? console.log("colaborador senior") : console.log("colaborador diretor")
+//salary <3000 ? console.log ("colaborador junior") : salary >= 3000 && salary < 10000 ? console.log("colaborador senior") : console.log("colaborador diretor")
+
---
+++ java scripts. pt 3/operadores ternário/parte4.js
@@ -0,0 +1,21 @@
+/*parte 4 js
+INICIALIZAÇÃO = criar uma variavel  e colocar um valor pra ela
+CONDIÇÃO = quando o valor for true , laço continuara iterando .ele ira verificar antes de cada interação
+expressão final = o que ira ocorrer a cada vez nosso laço*/
+
+/*for ([inicialiçao];[condição];[expreção final]){
+    seu codigo aqui
+
+    for (i =0 ; i  < 15 ; i++){
+        console.log(i)
+    }*/
+
+const users =
+    "maria "
+"joão"
+"cris"
+"antonio"
+"francisco"
+
+
+for (let i = 0; i < users.length; i++) { console.log(users[i])  }
