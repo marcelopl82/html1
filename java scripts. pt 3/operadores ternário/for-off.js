fd7657533407e6eee1fb0076dff7e77bc9188324
marcelopl82 <marceo.8468@gmail.com>
2024-07-17 20:53:56 -0300

    for of 17/07/24

---
+++ java scripts. pt 3/operadores ternário/for-off.js
@@ -0,0 +1,14 @@
+//for- of
+//estrutura de repetição//
+
+const myname="marcelo"//string//
+const users=["carla","nicollas","fernada"]//array//
+
+for(const letter of myname){
+    console.log(letter);
+}
+
+
+
+
+
