3ec31cfbc38d3f437d3c51c644abe30b7c6180f0
marcelopl82 <marceo.8468@gmail.com>
2024-07-30 20:35:16 -0300

    mario transação form. 30/07/24

---
+++ assets/mario/scriptsmario.js
@@ -0,0 +1,12 @@
+
+const fale = document.querySelector(".fale-conosco")
+
+
+
+function cliqueinobotão() {
+ form  .style.left = 50 %
+  form .style.transform; "translateX(-50%)"
+form .style.visibity = "visible"
+}
+
+
