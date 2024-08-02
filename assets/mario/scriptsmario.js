0eac02ecfc008f274855aa94b5d91efc0895bf17
marcelopl82 <marceo.8468@gmail.com>
2024-08-02 19:51:29 -0300

    autaraçoes 2/08/24

--- assets/mario/scriptsmario.js
+++ assets/mario/scriptsmario.js
@@ -1,12 +1,20 @@
 
-const fale = document.querySelector(".fale-conosco")
+const form = document.querySelector(".fale-conosco")
+const mascara = document.querySelector(".mascara-form")
 
 
-
-function cliqueinobotão() {
- form  .style.left = 50 %
-  form .style.transform; "translateX(-50%)"
-form .style.visibity = "visible"
+function mostrarform() {
+  form.style.left = " 50%"
+  form.style.transform = "translateX(-50%)"
+  mascara.style.visibility = "visible"
 }
 
 
+
+
+ function esconderform() {
+  form.stile.left = "-322px"
+  form.style.transform = "translateX(0)"
+  mascara.style.visibility
+    = "hidden"
+}
