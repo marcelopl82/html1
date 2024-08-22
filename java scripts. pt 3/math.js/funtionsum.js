4ae4efce2e1e9142bfe4296216779c322d289361
marcelopl82 <marceo.8468@gmail.com>
2024-08-22 20:40:28 -0300

    aula desconto em an 01

--- java scripts. pt 3/math.js/funtionsum.js
+++ java scripts. pt 3/math.js/funtionsum.js
@@ -1,1 +1,41 @@
-function sum(number1,number2){console.log(number1+number2)}
+
+const number = 0
+
+/*function sum(number1,number2){console.log(number1+number2)}
+sum(20,30)
+
+
+function sub(number1,number2,number3){console.log(number1-number2+number3)}
+sub(100,20,30)
+
+function div(number1,number3,number2){console.log(`result:${number1/number3/number2}`)}
+ div(150,50,2)
+    
+ function sum(){
+
+
+    return " oláboanoite"}
+
+console.log( sum())*/
+
+function calculatdiscount(price, descount) {
+    const result = (price * descount) / 100
+    return result
+}
+
+
+
+const cart = [10, 65, 150, 90, 40]
+let valuefinal = 0
+
+cart.forEach(value => {
+    valuefinal = valuefinal + (value - discount)
+
+    if (value > 65) { const discount = calculatdiscount(value, 10) 
+    valuefinal = valuefinal + (value - discount)}
+
+
+  else {  valuefinal+=value}
+
+});
+console.log(valuefinal)
