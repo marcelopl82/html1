5a59af0f11673a6abc3c475ac869e82e6bd63bed
marcelopl82 <marceo.8468@gmail.com>
2024-09-02 20:33:20 -0300

    mudança do nome da pasta 02/09/24

---
+++ java scripts. pt 3/math.js/jokenpow/script.js
@@ -0,0 +1,46 @@
+
+const result = document.querySelector(".result")
+const humanscore = document.querySelector("#human-score")
+const machinescore = document.querySelector("#Machine-score")
+let scorehumannumber = 0
+let scoremachinenumber = 0
+
+console.log(result)
+const playhuman = (humanchoice) => {
+      console.log(humanchoice)
+
+      playthegame(humanchoice, playMachine())
+
+}
+
+const playMachine = () => {
+      const choices = ['rock', 'paper', 'scissors']    //choice =escolhas//
+
+      const randomNumber = Math.floor(Math.random() * 3)
+
+      return choices[randomNumber]
+}
+const playthegame = (human, Machine) => {
+
+      console.log('humano: ' + human + 'Machine:' + Machine)
+
+      if (human === Machine){ result.innerHTML = "deu empate !" }
+      
+      else if ((human === "paper" && Machine === "rock") ||
+            (human === "rock" && Machine === "scissors ") ||
+            (human === "scissors " && Machine === "paper")
+      ) {
+            scorehumannumber++
+            humanscore.innerHTML = scorehumannumber
+            result.innerHTML = 'você ganhau !'
+      }
+
+      else {
+            scoremachinenumber++
+            machinescore.innerHTML = scoremachinenumber
+
+            result.innerHTML = "você perdeu para o pc !"
+      }
+
+}
+/* papel-> pedra-> tesoura*/
