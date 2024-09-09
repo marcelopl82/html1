0bbf33c845781e840bd49ff8cd735f5407160b1b
marcelopl82 <marceo.8468@gmail.com>
2024-09-09 19:55:48 -0300

    aula map 1 09/09/24

--- java scripts. pt 3/math.js/map/mapinicio.js
+++ java scripts. pt 3/math.js/map/mapinicio.js
@@ -6,13 +6,41 @@
 aceita 3 parametros
 - item do array
 - index
--array completo*/
+-array completo
 
 const number = [1, 2, 3, 4, 5, 6, 7]
-const double = number.Map(Item => {
-    const newItem = Item * 2
-    return newItem
-})
-console.log(number)
-console.log(double)
 
+const double = number.map((item) => item * 2)
+
+
+const list = [
+    { name: 'marcelo', vip: true },
+    { name: 'fernanda', vip: false },
+    { name: 'nicollas', vip: true },
+    { name: 'camila', vip: true },
+]
+const newlist = list.map(user => {
+    return
+    const newuser = {
+        name: user.name,
+        braceletcolor: user.vip ? 'black' : 'blue'
+    }
+
+
+})
+console.log(newlist)*/
+
+const studants = [
+    { name: 'marcelo', testgrade: 7 },
+    { name: 'fernanda', testgrade: 6 },
+    { name: 'nicollas', testgrade: 9 },
+    { name: 'camila', testgrade: 3 },
+]
+
+const newstudants = studants.map(studants => {
+    return {
+        name: studants.name,
+        statustudant: studants.testgrade >= 5 ? "aprovado" : "reprvado"
+    }
+})
+console.log(newstudants)
