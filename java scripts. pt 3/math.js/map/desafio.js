8309733f5e7188607cc7804c157f6e0cb8833216
marcelopl82 <marceo.8468@gmail.com>
2024-09-14 20:02:35 -0300

    aula desafio map ,fi.,redu. 14/09/24

---
+++ java scripts. pt 3/math.js/map/desafio.js
@@ -0,0 +1,14 @@
+const compannylist = [
+    { name: 'sansung', marketvalue: 50, ceo: 'kin hyu suk', foundedon: 1938 },
+    { name: 'microsoft', marketvalue: 415, ceo: 'satya nadella', foundedon: 1975 },
+    { name: 'intel', marketvalue: 117, ceo: 'brian krzanich', foundedon: 1968, },
+    { name: 'facebook', marketvalue: 383, ceo: 'mark zuckerberg', foundedon: 2004 },
+    { name: 'spotify', marketvalue: 30, ceo: 'daniel ek', foundedon: 2006 },
+    { name: 'apple', marketvalue: 845, ceo: 'tim cook', foundedon: 1976 },]
+
+
+const company = compannylist.map(acc, company => {
+    return
+    { compannylist.marketvalue - 10 }
+}, 0)
+console.log(company)
--- java scripts. pt 3/math.js/map/filter.js
+++ java scripts. pt 3/math.js/map/filter.js
@@ -1,10 +1,10 @@
 const listcarleilão = [
-    { name: 'Pegeot', age: 1999, fipevalue: 12.000 },
+    { name: 'Peugeot', age: 1999, fipevalue: 12.000 },
     { name: 'vw gol', age: 2007, fipevalue: 13.000 },
     { name: 'mercedes', age: 2004, fipevalue: 12.500 },
     { name: 'fiat uno vivace', age: 2011, fipevalue: 24.300 },
     { name: 'ford  fiesta', age: 2009, fipevalue: 18.200 },
-    { name: 'Pegeot', age: 1999, fipevalue: 12.000 },
+    { name: 'Peugeot', age: 1999, fipevalue: 12.000 },
 
 ]
 const newlist=listcarleilão.filter(listcarleilão=>{
