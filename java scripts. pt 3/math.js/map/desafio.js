65c47802f9ea12ab542ee6827a5438e42a6d4db9
marcelopl82 <marceo.8468@gmail.com>
2024-09-16 19:48:40 -0300

    desafio aula1 16/09

--- java scripts. pt 3/math.js/map/desafio.js
+++ java scripts. pt 3/math.js/map/desafio.js
@@ -6,9 +6,18 @@ const compannylist = [
     { name: 'spotify', marketvalue: 30, ceo: 'daniel ek', foundedon: 2006 },
     { name: 'apple', marketvalue: 845, ceo: 'tim cook', foundedon: 1976 },]
 
+const sub10percent = compannylist => {
+    compannylist.marketvalue = compannylist.marketvalue - compannylist.marketvalue / 10
+    return compannylist
+}
+const foundedon = (compannylist) => compannylist.foundedon < 1999
+const valuetotal = (acc, compannylist) => acc + compannylist.marketvalue
 
-const company = compannylist.map(acc, company => {
-    return
-    { compannylist.marketvalue - 10 }
-}, 0)
-console.log(company)
+
+
+const company = compannylist.map(sub10percent)
+
+    .filter(foundedon)
+    .reduce(valuetotal, 0)
+
+console.log(` the value  total was : $  ${company} `)
